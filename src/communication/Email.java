d6cf98f48d908d47db05715aa41eb1b74eb12e83
Rodolfo Arnaldo <rgnsa@iscte-iul.pt>
2018-03-13 16:37:06 +0000

    Email class separated from EmailSender

---
+++ src/communication/Email.java
@@ -0,0 +1,15 @@
+package communication;
+
+public class Email {
+
+	private String from;
+	private String to;
+	private String admin;
+	private String subject;
+	private String messageBody;
+	private String fileName;
+	
+	public Email() {
+		from = "geral.nemesis@gmail.com";
+	}
+}
