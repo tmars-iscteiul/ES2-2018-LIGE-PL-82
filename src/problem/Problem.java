5aa615535f8c7ed21dd1a280d80d8c9df51598be
Rodolfo Arnaldo <rgnsa@iscte-iul.pt>
2018-03-14 14:11:27 +0000

    Added some commentary to Problem class

--- src/problem/Problem.java
+++ src/problem/Problem.java
@@ -1,7 +1,7 @@
 package problem;
 
 public class Problem {
-	
+	//Class atributes must be determined from .xml file with the problem defined by the user
 	
 	public void load_problem (String file) {
 		
