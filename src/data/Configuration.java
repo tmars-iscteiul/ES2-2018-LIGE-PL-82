fa7f7aea519fb7c5aee363106dda5a42218421ad
rmnte-iscteiulpt <32777900+rmnte-iscteiulpt@users.noreply.github.com>
2018-04-23 14:39:01 +0100

    Updated data structures
    The old variables are now defined in a single configuration class, that
    holds important information that needs to be interpreted by the
    algorithm, such as value limits, and configuration size.

---
+++ src/data/Configuration.java
@@ -0,0 +1,35 @@
+package data;
+
+public class Variable {
+	
+	private String name;	/** Name of the variable*/
+	
+	private float upperLimit;	/** Upper bound for the float value*/
+	private float lowerLimit;	/** Lower bound for the float value*/
+	//private ArrayList<String> restrictions;
+	private Object[] valueArray;
+
+	protected Variable(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Sets the bounds of the value variable. 
+	 * The end points are included in the value intervals [lowerLimit, upperLimit]
+	 * @param low The lower limit
+	 * @param up The upper limit
+	 */
+	public void setLimits(float low, float up)	{
+		lowerLimit = low;
+		upperLimit = up;
+	}
+	
+	public void setArray(Object[] array)	{
+		valueArray = array;
+	}
+	
+	public Object[] getValueArray()	{
+		return valueArray;
+	}
+	
+}
