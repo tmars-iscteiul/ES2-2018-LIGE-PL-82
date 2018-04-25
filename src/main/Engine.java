500db1d8aca71e2f0a3a3ee5f80e9b0bfab20a2a
rmnte-iscteiulpt <32777900+rmnte-iscteiulpt@users.noreply.github.com>
2018-04-25 15:25:34 +0100

    Updated engine with placeholder problems

--- src/main/Engine.java
+++ src/main/Engine.java
@@ -1,10 +1,14 @@
 package main;
 
+import java.util.ArrayList;
+
 import data.Configuration;
 import data.Restriction;
 import data.problem.Problem;
 import data.problem.ProblemInputs;
 import data.problem.ProblemIntroduction;
+import data.problem.ProblemOptimization;
+import utils.Algorithms;
 import utils.TimeVariable;
 
 /**
@@ -23,21 +27,19 @@ public class Engine extends Thread	{
 		
 	}
 	
+	/**
+	 * Receives input from WEB GUI through JSON to run JMETAL'S ALGORITHMS. Sends results through email.
+	 *   Imports JSON and reads it (needs JSON's final structure), returning a PorlbmeInputs class, with Configurations List.
+	 *   This list will hold the information to be passed to the JMETAL algorithms
+	 *   Splits every algorithm run with jMetalHandler, using data.ProblemInputs's configuration list
+	 *   Executes every algorithm (jMetalHandler, after given the right inputs)
+	 *   During execution, jMetalHandler calls EmailSender to send emails on progress (Progress, ETA and other stats...)
+	 *   After generating, saves results (how? TBD) and sends to email.
+	 *   Closes.
+	 */
 	@Override
 	public void run()	{
 		readJson();
-		
-		/*
-		 * Receives input from WEB GUI through JSON to run JMETAL'S ALGORITHMS. Sends results through email.
-		 *   Imports JSON and reads it (needs JSON's final structure), returning a PorlbmeInputs class, with Configurations List.
-		 *   This list will hold the information to be passed to the JMETAL algorithms
-		 *   Splits every algorithm run with jMetalHandler, using data.ProblemInputs's configuration list
-		 *   Executes every algorithm (jMetalHandler, after given the right inputs)
-		 *   During execution, jMetalHandler calls EmailSender to send emails on progress (Progress, ETA and other stats...)
-		 *   After generating, saves results (how? TBD) and sends to email.
-		 *   Closes.
-		 *   
-		 */
 		
 	}
 	
@@ -57,18 +59,17 @@ public class Engine extends Thread	{
 		pinputs.addRestriction(new Restriction("Can't be 0", "!=", 0));
 		problem.setInputs(pinputs);
 		
+		ArrayList<Algorithms> algorithmList = new ArrayList<Algorithms>();
+		algorithmList.add(Algorithms.nsgaii);
+		ProblemOptimization poptimization = new ProblemOptimization(algorithmList);
+		problem.setOptimization(poptimization);
+		
 		// Fitness app? Just make a place holder one to calculate the example ones.
 		// Feedback not necessary
-		
 		
 	}
 	
 	public void executeAlgorithm() {
-		//DoubleConfiguration();
 	}
 
-	
-	
-	
-	
 }
