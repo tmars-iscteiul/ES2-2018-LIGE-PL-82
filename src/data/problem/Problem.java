ce2d794a4d7a4e0e5bf61079d1e850235dcc537b
rmnte-iscteiulpt <32777900+rmnte-iscteiulpt@users.noreply.github.com>
2018-04-25 14:32:32 +0100

    Updated problem data structure with new sub-classes

--- src/data/problem/Problem.java
+++ src/data/problem/Problem.java
@@ -6,6 +6,24 @@ import data.Configuration;
 import stakeholders.User;
 import sun.util.calendar.LocalGregorianCalendar.Date;
 
+/**
+ * The class problem will be the entire submission from the user. It will then fork into different sections, that will allow the engine to work
+ * with each one in a more intuitive way. 
+ * The hierarchy will follow the following tree:
+ * 
+ * Problem
+ * | - @see ProblemIntroduction
+ * | - @see ProblemInputs
+ * | | - @see Configuration 
+ * | | - @see * 
+ * | | - @see Restriction 
+ * | - @see ProblemFitnessApp TODO: Still unknown how we'll implement this. What link can we use? How do we download the .jar file? ...
+ * | - @see ProblemFeedback
+ * 
+ * 
+ * @author skner
+ *
+ */
 public class Problem {
 	//Class attributes must be determined from .JSON file with the problem defined by the user
 	
--- src/data/problem/ProblemInputs.java
+++ src/data/problem/ProblemInputs.java
@@ -1,27 +1,52 @@
 package data.problem;
 
-import data.Configuration;
+import java.util.ArrayList;
 
+import data.*;
+
+/**
+ * ProblemInputs will contain the inputs for the problem. It will be the most important class that the Jmetalhandler will work with.
+ * This class contains all the information necessary for the algorithms to run, including lists rules and restrictions.
+ * 
+ * @author skner
+ *
+ */
 public class ProblemInputs {
 
 	/*
 	 * inputs
 	 * - undefined {list-name, type, number-var, min-value, max-value, description}
+	 * - createInputItems {list, description} TODO
+	 * - restrictions {variable-name, symbol, value} Done, but don't understand how it will relate to other classes
 	 * 
 	 */
-
-	private Configuration config;
-	private String description;
 	
-	public ProblemInputs(String listName, String type, int size, float minValue, float maxValue, String description)	{
-		if (type == "int")	{
-			config = new Configuration(listName, minValue, maxValue, type, size);
-			this.description = description;
-		}
+	private ArrayList<Configuration> configList; // Queue?
+	private String description;
+	private ArrayList<Restriction> restrictionList;
+	
+	public ProblemInputs(String description)	{
+		this.description = description;
+	}
+	
+	public void addRestriction(String variableName, String symbol, int value) {
+		restrictionList.add(new Restriction(variableName, symbol, value));
+	}
+	
+	public void addRestriction(Restriction r)	{
+		restrictionList.add(r);
+	}
+	
+	public void addConfiguration(String listName, String type, int size, float minValue, float maxValue) {
+		configList.add(new Configuration(listName, minValue, maxValue, type, size));
+	}
+	
+	public void addRestriction(Configuration c)	{
+		configList.add(c);
 	}
 
-	public Configuration getConfig() {
-		return config;
+	public ArrayList<Configuration> getConfigList() {
+		return configList;
 	}
 	
 	
