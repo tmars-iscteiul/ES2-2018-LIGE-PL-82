f65f0df216372040d6cb9b54f368a4829ff26cee
rmnte-iscteiulpt <32777900+rmnte-iscteiulpt@users.noreply.github.com>
2018-05-10 02:40:21 +0100

    Linked Spring application with Engine
    Engine is now a thread, running under the spring service (server), that
    will interact directly with the web application

--- src/spring/controller/SpringController.java
+++ src/spring/controller/SpringController.java
@@ -3,12 +3,14 @@
  */
 package spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import data.problem.Problem;
+import main.Engine;
 
 /**
  * @author skner
@@ -17,20 +19,17 @@ import data.problem.Problem;
 @RestController
 public class SpringController {
 
-	/**
-	 * Pelo que precebi, com post ele precisa de ter o servidor ativo, e precisa de ser enviado um POST com o json (Content-Type: Application/json)
-	 * O servidor spring recebe o json e coloca automaticamente numa class problem. Adiciona assim à lista que terá de ser entregue ao engine (done)
-	 * 
-	 * 
-	 * TODO
-	 * Verificar se é necessario uma classe bruta para colocar o importado do json pelo spring (possivelmente)
-	 * O servidor spring fica ligado durante o processo de optimização, nao é possivel fechar o mesmo e correr o engine num outro thread
-	 * Como passar o objecto JSON para o engine? A classe main corre sequencialmente e nao devolve objetos
-	 * 
-	 * @param problem Classe criada automaticamente pelo Spring com a estrutura do JSON
-	 */
+	@Autowired
+	private Engine engine;
+	
+	@RequestMapping("/engine_status")
+	public String getListen()	{
+		return engine.getStatus();
+	}
+	
 	@RequestMapping(method = RequestMethod.POST, value = "/problem")
-	public void getProblemFromJson(@RequestBody Problem problem)	{
+	public void addProblem(@RequestBody Problem problem)	{
 		
 	}
+
 }
