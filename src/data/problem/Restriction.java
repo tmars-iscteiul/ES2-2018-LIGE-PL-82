605a2538e8ac766f815dce1aedbdff5a9b2dc498
rmnte-iscteiulpt <32777900+rmnte-iscteiulpt@users.noreply.github.com>
2018-05-16 00:38:10 +0100

    Added submission constructor to restriction class

--- src/data/problem/Restriction.java
+++ src/data/problem/Restriction.java
@@ -1,24 +1,28 @@
-package data;
+package data.problem;
 
 /**
  * Restrictions mean that in a linked configuration, no slot can ignore the restriction. 
- * 
- * @author skner
+ * TODO: Perhaps do some sort of automated method so the algorithms can more easily call the restrictions
+ *  
+ *  @author skner
  *
  */
 public class Restriction {
 
-	/*
-	 * restrictions {variable-name, symbol, value}
-	 */
-	
 	private String variableName;
 	private String symbol;
-	private int value;
+	private double value;
 	
-	public Restriction(String variableName, String symbol, int value)	{
+	public Restriction(String variableName, String symbol, double value)	{
 		this.variableName = variableName;
 		this.symbol = symbol;
 		this.value = value;
 	}
+	
+	public Restriction(data.submission.RestrictionsList restriction)	{
+		variableName = restriction.getVariableName();
+		symbol = restriction.getSymbol();
+		value = restriction.getValue(); 
+	}
+	
 }
