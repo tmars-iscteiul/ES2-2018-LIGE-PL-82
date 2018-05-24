77d84d49745c6f639b5c543ce1dc896a8f8b90ce
Tiago Rodrigues <tmars@iscte-iul.pt>
2018-05-24 19:22:59 +0100

    update chartjs

--- chartjs-app/src/app/app.component.ts
+++ chartjs-app/src/app/app.component.ts
@@ -34,21 +34,47 @@ export class AppComponent {
     body: problemName,
     mode: 'cors'
     })
-    .then((response) => response.text())
+    .then((response) => response.json())
     .then((response) => {
       console.log(response)
-
-      response.getJSON(response, function(json) {
-        var labels = json.map(function(item) {
-          return item.problemName;
-        })
-
-      }
-
-
-        
-
+      console.log(response.problemName)
     })
+      
+      
+      //console.log(r['problemName'])
+
+/*
+      var data = {
+        labels: 'Teste',
+        datasets: [
+          {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+          },
+          {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+          }
+        ]
+      };
+
+      var ctx = document.getElementById("myChart").getContext("2d");
+      ctx.canvas.width = 1000;
+      ctx.canvas.height = 800;
+
+      var myChart = new Chart(ctx).Bar(data);
+*/
+      })
+})
+  }
    
 
    // Loading data from json
@@ -87,47 +113,6 @@ export class AppComponent {
       var myChart = new Chart(ctx).Bar(data);
     });
     */
-    
-    // Bar Chart:
-    this.BarChart = new Chart('barChart', {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [9, 7, 3, 5, 2, 10],
-          backgroundColor: [
-            'rgba(255,99,132,0.2)',
-            'rgba(54,162,235,0.2)',
-            'rgba(255,206,86,0.2)',
-            'rgba(255,99,132,0.2)',
-            'rgba(255,100,132,0.2)',
-            'rgba(255,101,132,0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,0.2)',
-            'rgba(54,162,235,0.2)',
-            'rgba(255,206,86,0.2)',
-            'rgba(255,99,132,0.2)',
-            'rgba(255,100,132,0.2)',
-            'rgba(255,101,132,0.2)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        title: 'Bar Chart',
-        display: true
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    });
-  }
 
   // Radar Chart
 
